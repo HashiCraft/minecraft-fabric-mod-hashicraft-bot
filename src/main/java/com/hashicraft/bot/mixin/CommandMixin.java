14006519d3b7ba4374a422db048e98a47f9c6e46
Nic Jackson <jackon.nic@gmail.com>
2021-01-10 18:41:56 +0000

    Add validation

--- src/main/java/com/hashicraft/bot/mixin/CommandMixin.java
+++ src/main/java/com/hashicraft/bot/mixin/CommandMixin.java
@@ -2,15 +2,21 @@ package com.hashicraft.bot.mixin;
 
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.LiteralText;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.concurrent.CompletableFuture;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+
+import com.google.gson.JsonParser;
+import com.google.gson.JsonElement;
 
 import static net.minecraft.server.command.CommandManager.literal;
 import org.spongepowered.asm.mixin.Final;
@@ -29,6 +35,8 @@ public class CommandMixin {
 
   private String botlocation = System.getenv("BOT_ADDRESS");
   private String botAPIKey = System.getenv("BOT_API_KEY");
+  
+  private String activeBot = "";
 
   @Inject(method = "<init>", at = @At("RETURN"))
   private void onRegister(CommandManager.RegistrationEnvironment arg, CallbackInfo ci) {
@@ -50,9 +58,18 @@ public class CommandMixin {
   private LiteralArgumentBuilder<ServerCommandSource> startArgs() {
     return literal("start").executes(context -> {
       CompletableFuture.runAsync(() -> {
-          System.out.println("bot_start");
+          System.out.println("bot_start: " + this.activeBot);
+          if(this.activeBot != "") {
+            context.getSource().sendError(new LiteralText(
+                "A bot is already running on the server, " +
+                "use the kill command to remove it before creating a new bot"));
+
+            return;
+          }
+
           try {
-            startCommand();
+            String id = startCommand();
+            this.activeBot = id;
           } catch (IOException e) {
             e.printStackTrace();
           }
@@ -65,9 +82,14 @@ public class CommandMixin {
   private LiteralArgumentBuilder<ServerCommandSource> killArgs() {
     return literal("kill").executes(context -> {
       CompletableFuture.runAsync(() -> {
+          if (this.activeBot == "") { 
+            return;
+          }
+
           System.out.println("bot_kill");
             try {
-              killCommand();
+              killCommand(this.activeBot);
+              this.activeBot = "";
             } catch (IOException e) {
               e.printStackTrace();
             }
@@ -77,7 +99,7 @@ public class CommandMixin {
       });
   }
 
-  private void startCommand() throws IOException {
+  private String startCommand() throws IOException {
     URL u = new URL(botlocation + "/bot");
     HttpURLConnection conn = (HttpURLConnection) u.openConnection();
     conn.setDoOutput(true);
@@ -87,13 +109,25 @@ public class CommandMixin {
     conn.setConnectTimeout(3000);
     conn.setReadTimeout(30000);
     
-    InputStream is = conn.getInputStream(); 
-
     System.out.println(conn.getResponseCode());
+    
+    InputStream is = conn.getInputStream(); 
+    Reader reader = new InputStreamReader(is);
+
+    JsonParser jsonParser = new JsonParser();
+    JsonElement je = jsonParser.parse(reader);
+
+    String id = je.getAsJsonObject().get("id").getAsString();
+
+    System.out.println("response " + je.toString());
+    System.out.println("id " + id);
+
+    return id;
   }
 
-  private void killCommand() throws IOException {
-    URL u = new URL(botlocation + "/bot/mybot");
+  private void killCommand(String botID) throws IOException {
+
+    URL u = new URL(botlocation + "/bot/" + botID);
     HttpURLConnection conn = (HttpURLConnection) u.openConnection();
     conn.setDoOutput(true);
     conn.setRequestMethod("DELETE");
